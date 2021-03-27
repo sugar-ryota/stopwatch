7460071b032b0de7a2c4704d05bfce974d83995c
sugar-ryota <s1811345@s.tsukuba.ac.jp>
2021-03-27 15:47:31 +0900

    header部分をスタリリングしました

--- App.jsx
+++ App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.timerHeader}>
+        <View>
+          <Text style={styles.timer}>00:00.00</Text>
+        </View>
+      </View>
+      <View>
+        <Text>Start</Text>
+      </View>
+      {/* eslint-disable-next-line */}
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -12,8 +22,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // 最後に黒にする
+    backgroundColor: 'green',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  timerHeader: {
+    backgroundColor: '#000000',
+    height: 375,
+    // 上下方向
+    justifyContent: 'flex-end',
+    width: '100%',
+    alignItems: 'center',
+  },
+  timer: {
+    backgroundColor: '#000000',
+    color: '#ffffff',
+    fontSize: 90,
+    lineHeight: 100,
+    fontWeight: '200',
+    marginBottom: 80,
+    width: '100%',
   },
 });
